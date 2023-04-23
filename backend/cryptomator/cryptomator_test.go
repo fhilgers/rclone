233a64bbfb2aa00db7a0a7cc559f9921f4885456
Felix Hilgers <felix_hilgers@posteo.de>
2023-04-23 05:33:00 +0200

    test(backend/cryptomator): add unimplementablefsmethods

--- backend/cryptomator/cryptomator_test.go
+++ backend/cryptomator/cryptomator_test.go
@@ -1,4 +1,4 @@
-// Test Crypt filesystem interface
+// Test Cryptomator filesystem interface
 package cryptomator_test
 
 import (
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/rclone/rclone/backend/cryptomator"
 	"github.com/rclone/rclone/fs/config/obscure"
 	"github.com/rclone/rclone/fstest"
 	"github.com/rclone/rclone/fstest/fstests"
@@ -13,7 +14,6 @@ import (
 	_ "github.com/rclone/rclone/backend/alias"
 	_ "github.com/rclone/rclone/backend/local"
 	_ "github.com/rclone/rclone/backend/s3"
-	_ "github.com/rclone/rclone/backend/sftp"
 )
 
 // TestIntegration runs integration tests against the remote
@@ -23,27 +23,29 @@ func TestIntegration(t *testing.T) {
 	}
 	fstests.Run(t, &fstests.Opt{
 		RemoteName:                   *fstest.RemoteName,
-		UnimplementableFsMethods:     []string{"OpenWriterAt"},
+		NilObject:                    (*cryptomator.Object)(nil),
+		UnimplementableFsMethods:     []string{"Purge", "ChangeNotify", "MergeDirs", "ListR", "OpenWriterAt"},
 		UnimplementableObjectMethods: []string{"MimeType"},
-		QuickTestOK:                  true,
 		TiersToTest:                  []string{"REDUCED_REDUNDANCY", "STANDARD"},
 	})
 }
 
+// TestStandard runs integration tests against the remote
 func TestStandard(t *testing.T) {
 	if *fstest.RemoteName != "" {
 		t.Skip("Skipping as -remote set")
 	}
-	tempdir := filepath.Join(os.TempDir(), "rclone-crypt-test-standard")
+	tempdir := filepath.Join(os.TempDir(), "rclone-cryptomator-test-standard")
 	name := "TestCryptomator"
 	fstests.Run(t, &fstests.Opt{
 		RemoteName: name + ":",
+		NilObject:  (*cryptomator.Object)(nil),
 		ExtraConfig: []fstests.ExtraConfigItem{
 			{Name: name, Key: "type", Value: "cryptomator"},
 			{Name: name, Key: "remote", Value: tempdir},
 			{Name: name, Key: "password", Value: obscure.MustObscure("potato")},
 		},
-		UnimplementableFsMethods:     []string{"OpenWriterAt"},
+		UnimplementableFsMethods:     []string{"Purge", "ChangeNotify", "MergeDirs", "ListR", "OpenWriterAt"},
 		UnimplementableObjectMethods: []string{"MimeType"},
 		QuickTestOK:                  true,
 	})
